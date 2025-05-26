957f697d752e103006619a1cfcc4fe5cae4febb8
Murphy Ochuba <mnochuba@gmail.com>
2025-05-26 23:15:18 +0100

    Moved services to infrastructure project

--- SmartWebScraper.Infrastructure/Services/BingScraper.cs
+++ SmartWebScraper.Infrastructure/Services/BingScraper.cs
@@ -1,14 +1,7 @@
-﻿using SmartWebScraper.Domain.Contracts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 using System.Web;
 
-namespace SmartWebScraper.Application.Services;
+namespace SmartWebScraper.Infrastructure.Services;
 public class BingScraper //: ISearchEngineScraper
 {
     private readonly HttpClient _httpClient;
